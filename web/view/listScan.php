09fb1634b543939477aae6cac9f14a0c3b727ddb
Virgile PARAT <virgile.parat@live.fr>
2017-05-29 17:58:07 +0200

    Work in progress

--- web/view/listScan.php
+++ web/view/listScan.php
@@ -1,8 +1,73 @@
 <?php
-  //permet de vérifier la bonne connexion de l'utilisateur
+//permet de vérifier la bonne connexion de l'utilisateur
 if (isset($_SESSION['identifiant'])) {
 
-  $get_all_ref = $pdo->query("SELECT id_article, ref_article FROM article");
+  /************Variables**********/
+
+  $cur_day_date = date("d/m/Y") ;
+
+  $week_start = date('d/m/Y', strtotime('-'. date('w') .' days'));
+
+  $cur_month_date = date("01/m/Y") ;
+
+  $cur_year_date = date("01/01/Y") ;
+
+
+  /**************SQL**************/
+  //Permet de récupérer les articles dans une liste déroulante pour afficher des infos sur un article particulier
+  $sql_all_ref = $pdo->query("SELECT id_article, ref_article FROM article");
+  //Recupère les articles actuellement au zingage
+
+  $sql_in_zingage = $pdo->query("
+    SELECT 
+      COUNT(scan.id_article) AS nb_article_is_zing,
+      ref_article,
+      nom_article,
+      SUM(article.poid_article) AS poids_cumule,
+      SUM(article.nb_article) AS quantite_cumule
+    FROM 
+      article 
+    LEFT JOIN 
+      scan ON article.id_article = scan.id_article
+    WHERE 
+      is_in_zingage = '1' 
+    GROUP BY
+      ref_article
+    ");
+
+  //Récupère l'article le plus ancien actuellement au zingage
+  $sql_oldest_in_zingage = $pdo->query("
+    SELECT 
+      ref_article,
+      MIN(scan.date_scan_depart) AS oldest_depart
+    FROM 
+      article 
+    LEFT JOIN 
+      scan ON article.id_article = scan.id_article
+    WHERE
+      scan.is_in_zingage = 1
+    LIMIT 1
+    ");
+
+  $sql_date_return = $pdo->query("
+    SELECT 
+      COUNT(scan.id_article) AS nb_article_is_zing,
+      ref_article,
+      nom_article,
+      SUM(article.poid_article) AS poids_cumule,
+      SUM(article.nb_article) AS quantite_cumule
+    FROM 
+      article 
+    LEFT JOIN 
+      scan ON article.id_article = scan.id_article
+    WHERE 
+      is_in_zingage = '0'
+    AND
+      scan.date_scan_retour > 20170501
+    GROUP BY
+      ref_article
+    ");
+
 ?>
   <div>
 
@@ -12,15 +77,15 @@ if (isset($_SESSION['identifiant'])) {
 
         <h4>Afficher l'historique d'un article</h4>
         <form method="get" action="<?= "http://" . $_SERVER['SERVER_NAME'] . "/zingage/" ?>">
-          <select class="selectpicker" data-style="btn-danger" data-live-search="true" multiple="multiple">
+          <select name="article" class="selectpicker" data-style="btn-primary" style="color: white;" data-live-search="true" multiple="multiple">
             <?php
             //Permet d'afficher la liste des refs dans un dropdown
-              while ($row = $get_all_ref->fetch()) {
+              while ($row = $sql_all_ref->fetch()) {
                 echo '<option value="' . $row['id_article'] . '">' . $row['ref_article'] . '</option>';
               }
             ?>
           </select>
-          <input type="submit" name="Envoyer">
+          <input type="submit" value="Afficher">
         </form>
 
       </div>
@@ -34,10 +99,41 @@ if (isset($_SESSION['identifiant'])) {
               <th>REFERENCE</th>
               <th>DESIGNATION</th>
               <th>POIDS CUMULE</th>
-              <th>QUANTITE UNITAIRE</th>
+              <th>QUANTITE CUMULE</th>
             </tr>
+            <?php
+            //Récupère les articles encore au zingage
+              while ($row = $sql_in_zingage->fetch()) 
+              {
+                echo "<tr>";
+                echo '<td>' . $row['nb_article_is_zing'] . '</td>';
+                echo '<td>' . $row['ref_article'] . '</td>';
+                echo '<td>' . $row['nom_article'] . '</td>';
+                echo '<td>' . $row['poids_cumule'] . '</td>';
+                echo '<td>' . $row['quantite_cumule'] . '</td>';
+                echo "</tr>";
+              } 
+            ?>
           </table>
-          <h3>L'envoie le plus vieux est la ref ... envoyé le ...</h3>
+            <?php
+            //Récupère l'article le plus ancien (individuel) dans la BDD
+            if ($sql_oldest_in_zingage->rowCount() != 0 ) {
+
+              while ($row = $sql_oldest_in_zingage->fetch()) 
+              {
+                $oldest_ref = $row['ref_article'];
+                $oldest_depart = $row['oldest_depart'];
+              } 
+            ?>
+          <div class="bg-info padding-all-small">  
+            <h3 class="center">L'article le plus ancien envoyé au zingage posséde la référence : </h3>
+            <h3 class="center"><b>"<?= $oldest_ref ?>"</b></h3>
+            <h3 class="center"> Il a été envoyé le </h3>
+            <h3 class="center"><b><?= date( "d/m/Y H\h i:s", strtotime($oldest_depart) ) ?></b> </h3>
+          </div>
+          <?php } ?>
+
+
 
       </div>
 
@@ -52,7 +148,7 @@ if (isset($_SESSION['identifiant'])) {
                 <th>REFERENCE</th>
                 <th>DESIGNATION</th>
                 <th>POIDS CUMULE</th>
-                <th>QUANTITE UNITAIRE</th>
+                <th>QUANTITE CUMULE</th>
               </tr>
             </table>
           </div>
@@ -65,20 +161,20 @@ if (isset($_SESSION['identifiant'])) {
                 <th>REFERENCE</th>
                 <th>DESIGNATION</th>
                 <th>POIDS CUMULE</th>
-                <th>QUANTITE UNITAIRE</th>
+                <th>QUANTITE CUMULE</th>
               </tr>
             </table>
           </div>
 
           <div>
             <table id="tab-list-article" class="table table-bordered table-striped table-responsive">
-            <h4 class="center">Année 2017</h4>
+            <h4 class="center">Année <?= date("Y") ?></h4>
               <tr>
                 <th>NOMBRE</th>
                 <th>REFERENCE</th>
                 <th>DESIGNATION</th>
                 <th>POIDS CUMULE</th>
-                <th>QUANTITE UNITAIRE</th>
+                <th>QUANTITE CUMULE</th>
               </tr>
             </table>
           </div>
